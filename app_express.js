77f694cf93f10b05fffa90351467f393823239fd
travel15740 <travel15740@gmail.com>
2023-10-12 16:47:26 +0800

    在 EXPRESS 專案增加頁碼功能, 並限制每頁顯示張數

--- app_express.js
+++ app_express.js
@@ -17,10 +17,61 @@ app.use(express.static("public"));
 const data_json = require("./public/data.json");
 const restaurants = data_json.restaurants;
 
+
+// 設計函式處理清單資料
+// #1 定義需要用到的變數
+const cards_per_page = 8;
+
+
+// #2 功能設計
+// 限制每頁顯示圖卡張數
+function getItemsByPage(data, page){
+  const startIndex = (page - 1) * cards_per_page;
+  const EndIndex = page * cards_per_page;
+  return data.slice(startIndex, EndIndex);
+}
+
+// 動態渲染網站頁數
+function renderPaginator(amount){
+  // 用圖卡數量決定網站頁數
+  const numberOfPages = Math.ceil(amount / cards_per_page);
+  console.log(`網站頁數: ${numberOfPages}`);
+
+  // 產生指定長度的空陣列
+  let pages = Array(numberOfPages);
+
+  // 動態產生網站頁數陣列
+  for(let i=0; i<numberOfPages; i++){
+    pages[i] = i + 1;
+  }
+  console.log(`動態產生網站頁數陣列: ${pages}`);
+  return pages;
+}
+
+
+// #3 呼叫函式產生所需要的變數
+// 產生頁碼器陣列
+const pages = renderPaginator(restaurants.length);
+
+// 在有page將資料進行分段的概念後，預設顯示第一段的陣列資料
+let page = 1;
+let restaurants_page = getItemsByPage(restaurants, page);
+
+
+
 // 設定路由與渲染網頁:首頁
 app.get("/", (req, res)=>{
   console.log(restaurants[0]);
-  res.render("index", {restaurants:restaurants})
+  res.render("index", {restaurants:restaurants_page, pages:pages})
+})
+
+
+// 設定路由與渲染網頁:首頁結合頁碼器渲染
+app.get("/page=:page", (req, res) => {
+  page = Number(req.params.page);
+  console.log(`點選頁碼: ${page}`);
+  restaurants_page = getItemsByPage(restaurants, page);
+  res.render("index", {restaurants:restaurants_page, pages:pages })
 })
 
 
@@ -28,13 +79,29 @@ app.get("/", (req, res)=>{
 app.get("/search", (req, res) => {
   // input 的 name="keyword" 會以?的形式帶入路由 
 
-  // 依餐廳名稱篩選(使用filter陣列方法)
-  const keyword = req.query.keyword;
-  const restaurants_filter = restaurants.filter(restaurant =>
-    restaurant.name.includes(keyword) ||
-    restaurant.category.includes(keyword));
+  
 
-  res.render("index", {restaurants:restaurants_filter,keyword:keyword});
+  // 依餐廳名稱篩選(使用filter陣列方法)
+  let keyword = req.query.keyword;
+  let restaurants_filter = restaurants.filter(restaurant =>
+    restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
+    restaurant.category.toLowerCase().includes(keyword.toLowerCase()));
+
+  let pages_filter = [];
+
+
+  // 追加檢查: if 搜尋表單的輸入值為空白,渲染搜尋前的變數
+  if (!keyword.length) {
+    restaurants_filter = restaurants_page;
+    pages_filter = pages;
+  }
+
+  // 追加檢查: if 搜尋表單沒有結果
+  if(restaurants_filter.length===0){
+    console.log(`無法找到含關鍵字${keyword}的項目`);
+  }
+
+  res.render("index", { restaurants: restaurants_filter, keyword: keyword, pages: pages_filter });
 })
 
 
